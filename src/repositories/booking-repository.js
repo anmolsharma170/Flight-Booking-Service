5b8bc90e7f1a409fc0e238760984a3e5d10b0ad8
AnmolSharma-git <148962440+AnmolSharma-git@users.noreply.github.com>
2026-05-19 16:02:17 +0530

    added booking logic with transactions

--- src/repositories/booking-repository.js
+++ src/repositories/booking-repository.js
@@ -5,7 +5,15 @@ class BookingRepository extends CrudRepository{
     constructor(){
         super(Booking);
     }
+    // we have to create a booking we wont be using create booking crud operation because we need to pass a transaction object than that we will be using here 
+
+    async createBooking(data,transaction){
+        const response = await Booking.create(data, {transaction:transaction});
+        return response;
+    }
 }
+
+
 module.exports={
     BookingRepository
 }
--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -5,19 +5,30 @@ const db = require('../models');
 const {ServerConfig} = require('../config');
 const AppError = require('../utils/errors/app-error');
 const { StatusCodes } = require('http-status-codes');
+const bookingRepository = new BookingRepository();
 async function createBooking(data){
-    return new Promise((resolve,reject)=>{
-        
-        const result = db.sequelize.transaction(async function bookingImpl(t){
-            const flight = await axios.get(`${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${data.flightId}`);
+    const transaction = await db.sequelize.transaction();
+    try {
+        const flight = await axios.get(`${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${data.flightId}`);
             const flightdata = flight.data.data;
             if(data.noOfSeats > flightdata.total_seats){
-                reject(new AppError('Required number of seats not available',StatusCodes.BAD_REQUEST));
+                throw new AppError('Required number of seats not available',StatusCodes.BAD_REQUEST);
             }
-            resolve(true);
-        });
-    })
+            const totalBillingAmount = data.noOfSeats * flightdata.price;
+            const bookingpayload = {...data, totalCost: totalBillingAmount}; //booking payload is just like a object that we are going to send
+            // we will be passsing the same payload in order to make booking
+            const booking  =  await bookingRepository.create(bookingpayload,transaction);
+            await axios.patch(`${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${data.flightId}/seats`,{seats: Number(data.noOfSeats), dec: true});
+
+
+
+            await transaction.commit();
+            return booking;
+    } catch (error) {
+        await transaction.rollback();
+        throw error;
     }
+}
     
 
 module.exports = {
